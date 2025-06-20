2e8667b81b08db1126ad939a17071f18fe87cea3
Arjun Chandel <arjunchandel910@gmail.com>
2025-06-20 20:34:27 +0530

    crate an authMiddleware to protect routes in the backend.

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,5 +1,22 @@
-export default authMiddleware = async (req, res) => {
-  const token = await req.headers("auth-token");
-  const decoded = jwt.verify(token, "shhhhh");
-  console.log(decoded);
+import jwt from "jsonwebtoken";
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    const token = req.headers["auth-token"];
+
+    if (!token) {
+      return res
+        .status(401)
+        .json({ error: "Access Denied. No token provided." });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.user = decoded;
+
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: "Invalid token." });
+  }
 };
+
+export default authMiddleware;
