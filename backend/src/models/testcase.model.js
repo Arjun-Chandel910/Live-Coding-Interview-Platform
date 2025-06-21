21a5c7716a569a2c77d4132f9825149599c0388f
Arjun Chandel <arjunchandel910@gmail.com>
2025-06-21 19:50:42 +0530

    create testcaseSchema.

--- backend/src/models/testcase.model.js
+++ backend/src/models/testcase.model.js
@@ -1,3 +1,21 @@
 import { Schema, model } from "mongoose";
 
-const testcaseSchema = new Schema({});
+const testcaseSchema = new Schema(
+  {
+    input: {
+      type: String,
+      required: true,
+    },
+    output: {
+      type: String,
+      required: true,
+    },
+    isHidden: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+const Testcase = model("Testcase", testcaseSchema);
+export default Testcase;
